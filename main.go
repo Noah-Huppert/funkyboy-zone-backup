2228df7e3d23b02ece227efae95b9f6f7425217c
Noah Huppert <contact@noahh.io>
2019-03-18 01:18:44 -0400

    made use Upload.Format configuration

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 
 	// {{{1 Tar file
 	// {{{2 Open tar file
-	fName := strftime.Format("backup-%Y-%m-%d-%H:%M:%S", time.Now())
+	fName := strftime.Format(cfg.Upload.Format, time.Now())
 	tarFPath := fmt.Sprintf("/var/tmp/%s.tar", fName)
 
 	tarF, err := os.Create(tarFPath)
