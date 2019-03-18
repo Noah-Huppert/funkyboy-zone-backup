931eb7cf5455f5d3346696b934699926093d9243
Noah Huppert <contact@noahh.io>
2019-03-18 00:36:28 -0400

    added logger argument to Backuper

--- backup/backuper.go
+++ backup/backuper.go
@@ -2,10 +2,12 @@ package backup
 
 import (
 	"archive/tar"
+
+	"github.com/Noah-Huppert/golog"
 )
 
 // Backuper performs the action of backing up a file.
 type Backuper interface {
 	// Backup files to w.
-	Backup(w *tar.Writer) error
+	Backup(logger golog.Logger, w *tar.Writer) error
 }
